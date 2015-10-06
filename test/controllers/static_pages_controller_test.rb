3ebb5ad63f75729975259ae4dff1a0962f0309b7
spectre6000 <spectre6000@gmail.com>
2015-10-06 12:35:19 -0600

    page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,13 +5,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    # assert_select "title", "Blognamehere"
+    assert_select "title", "Blognamehere"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    # assert_select "title", "About | Blognamehere"
+    assert_select "title", "About | Blognamehere"
   end
 
 end
