8a79ae1c63158aaeb9cc3d86871571d5887405c8
spectre6000 <spectre6000@gmail.com>
2015-10-07 10:35:34 -0600

    refactor page titles into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Page titles
+  def tab_title(page_title = '')
+    blog_title = "Blognamehere"
+    if page_title.empty?
+      blog_title
+    else
+      page_title + " | " + blog_title
+    end
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,16 +2,20 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   
+  def setup
+    @page_title = "Blognamehere"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Blognamehere"
+    assert_select "title", "#{@page_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Blognamehere"
+    assert_select "title", "About | #{@page_title}"
   end
 
 end
