15cf1ee4f3a6afa39ce1549040c873077a57a63c
spectre6000 <spectre6000@gmail.com>
2015-10-08 15:27:14 -0600

    refactor routes and generate integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  root                  'static_pages#home'
+  get   'about'     =>  'static_pages#about'
 end
